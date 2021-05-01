48a3b46293523cfaeb4da4e0337de68c404471df
singhmonika664 <70739193+singhmonika664@users.noreply.github.com>
2021-05-01 20:38:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var ball;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+}
+
+function draw(){
+    background("white");
+    
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0)    
+    }
+    if(keyDown(RIGHT_ARROW)){
+        changePosition(+1,0)
+    }
+    if(keyDown(UP_ARROW)){
+        changePosition(0,-1)
+    }
+    if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1)
+    }
+
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x=ball.x+x;
+    ball.y=ball.y+y;
+}
+
